ed361cb52d56f4bfaaccf2aea7dac01f70ef2f91
BlueDuck <145476276+ductri09072004@users.noreply.github.com>
2025-03-11 22:55:23 +0700

    delete: extractbill

--- src/routes/ExtractBill.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getRequests } from "../controllers/ExtractBill.controller.js";
-
-const router = express.Router();
-
-router.get("/extractbillsss", getRequests);
-
-// Xuất theo chuẩn ES Module
-export default router;
